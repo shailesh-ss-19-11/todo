d9336c37c5f21e9194096f424f9bb141de69fc56
shailesh-ss-19-11 <gokhaleshail@gmail.com>
2023-11-30 09:37:58 +0530

    todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,40 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './todolist.scss';
+import Swal from 'sweetalert2'
+import { MdOutlineDelete } from "react-icons/md";
 const TodoList = () => {
     const [inputValue, setinputValue] = useState("");
     const [todoList, settodoList] = useState([]);
-
+    const [show, setshow] = useState(false);
     const handleinputChange = (e) => {
         setinputValue(e.target.value)
     }
 
     const addTodo = () => {
-        console.log(inputValue)
-        let obj = {};
-        obj.id = todoList.length + 1;
-        obj.taskname = inputValue;
-        setinputValue("")
-        const newtodo = [...todoList, obj]
-        console.log(newtodo, "newtodo")
-        settodoList(newtodo)
+        if (inputValue == "") {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Please input some task',
+                icon: 'error',
+                confirmButtonText: 'okay'
+            })
+        } else {
+            let obj = {};
+            obj.id = todoList.length + 1;
+            obj.taskname = inputValue;
+            setinputValue("")
+            const newtodo = [...todoList, obj]
+            settodoList(newtodo)
+            localStorage.setItem("todolist", JSON.stringify(newtodo))
+        }
     }
 
+    const checkInput = (e, id, index) => {
+        let todolist = [...todoList];
+        const obj = todolist.find((task) => task.id == id);
+        obj.checked = e.target.checked;
+        todolist.splice(index, 1, obj)
+        settodoList(todolist)
+        localStorage.setItem("todolist", JSON.stringify(todolist))
+    }
+
+    useEffect(() => {
+        const todolist = JSON.parse(localStorage.getItem("todolist"));
+        settodoList(todolist)
+    }, [])
     return (
         <div className='container mt-5'>
-            <div className='input-todo m-auto'>
+            {show ? <div className='input-todo m-auto'>
                 <input type="text" onChange={handleinputChange} value={inputValue} />
                 <button className='btn btn-sm btn-primary' onClick={addTodo}>ADD</button>
-            </div>
+            </div> : null}
+            {/* <button className='btn btn-primary' onClick={() => setshow(!show)}>{show ? "hide" : "show"}</button> */}
             {/* <div>
                 <input type="text" />
                 <button>sort</button>
             </div> */}
             <ul className='todo-list'>
                 {todoList.length > 0 ?
-                    todoList.map((todo) => {
+                    todoList.map((todo, index) => {
                         return (
-                            <li className='d-flex '> <input type="checkbox" className='p-1' />
-                                <p className='m-0 p-1'>{todo.taskname}</p>
+                            <li className='d-flex justify-content-between' key={index}>
+                                <div className='d-flex align-items-center'>
+                                    <input type="checkbox" className='p-1' onChange={(e) => checkInput(e, todo.id, index)} checked={todo.checked ? true : false} />
+                                    {todo?.checked == true ?
+                                        <p className='m-0 p-1'><del>{todo.taskname}</del></p> :
+                                        <p className='m-0 p-1'>{todo.taskname}</p>
+                                    }
+                                </div>
+                                <button className='btn btn-sm btn-danger m-2'><MdOutlineDelete /></button>
                             </li>
                         )
                     }) : "there is no todo"}
